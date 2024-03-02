2bf0678cbe025f74edec77f6bf1d0382eb7f8485
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2024-03-02 02:31:02 +0100

    feature: init chatbot boilerplate resource

---
+++ src/controllers/chatbot.controller.js
@@ -0,0 +1,26 @@
+import { Router } from "express"
+
+const router = Router();
+
+router.get("/chatbot", (req, res) =>{
+
+    try {
+        
+        return res.status(200).json({ data: "resultData" });
+    } catch (error) {
+        console.error('Error in get:', error);
+        return res.status(500).json({ error: 'Internal Server Error.' });
+    }
+})
+
+router.post("/chatbot", (req, res) =>{
+    try {
+        
+        return res.status(200).json({ data: "resultData" });
+    } catch (error) {
+        console.error('Error in post:', error);
+        return res.status(500).json({ error: 'Internal Server Error.' });
+    }
+})
+
+export default router
